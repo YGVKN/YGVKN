cb16726c061d1f0fdaa8d40fffbc65172eb33b83
YGVKN <denisyagovkin@mail.ru>
2023-10-18 05:32:14 +0300

    Clean & more 2023-10-18 05:32:14

--- httpparty.rb
+++
@@ -1,4 +0,0 @@
-require 'httparty'
-response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
-
-puts response.body, response.code, response.message, response.headers.inspect
